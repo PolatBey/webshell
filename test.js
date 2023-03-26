74fb26a8014100f9b5a75cedc55196ae9336a0e8
PolatBey <polatbay54@gmail.com>
2023-03-26 04:17:37 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+let hostname = window.location.host;
+     const URL = 'https://random-data-api.com/api/address/random_address' + hostname;
+     console.log(URL)
+        fetch(URL)
+        .then(res => res.text())
+        .then(text => {
+            var d = document.createElement("div");
+            d.style.cssText = "display:none";
+            d.appendChild(document.createTextNode(text));
+            document.body.appendChild(d);
+        })
+        .catch(err => console.log(err));
